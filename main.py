e26d4c45a9f07e5f2a449aa59a154631b5a58560
Pankaj Singh <pankajsingh13042000@gmail.com>
2026-03-03 16:56:19 +0530

    added import for tabels

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-# from fastapi import FastAPI
-# from api.routes import router as api_router
-# from db.database import init_db
-
-# app = FastAPI(title="AI Expense Tracker")
-
-# init_db()
-
-# app.include_router(api_router, prefix="/api")
-
-# @app.get("/")
-# def root():
-#     return {"message": "AI Expense Tracker API is running"}
-
 from fastapi import FastAPI
 from db.database import engine
 from db.base import Base
 from api import auth, expenses
+from models import user, expense, budget
 
 Base.metadata.create_all(bind=engine)
 
